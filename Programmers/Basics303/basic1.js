df4b9dfa286a93c92cadacc66e7ef1aff16386dc
minzziPark <minzzi0068@gmail.com>
2024-02-28 03:47:02 +0900

    add : basic2 추가

--- Programmers/Basics303/basic1.js
+++ Programmers/Basics303/basic1.js
@@ -1,3 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120810
 function solution(num1, num2) {
   var answer = num1 % num2;
   return answer;
---
+++ Programmers/Basics303/basic2.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+function solution(num1, num2) {
+  var answer = num1 - num2;
+  return answer;
+}
