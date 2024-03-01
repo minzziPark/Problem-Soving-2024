681ad2aafbad75785e952bde592f656190866da6
minzziPark <minzzi0068@gmail.com>
2024-03-01 21:14:01 +0900

    add : basic54 추가

---
+++ Programmers/Basics303/Day4/basic54.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181944
+// 홀짝 구분하기
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  n = Number(input[0]);
+  n % 2 === 0 ? console.log(n, "is even") : console.log(n, "is odd");
+});
