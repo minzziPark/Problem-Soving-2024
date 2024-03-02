3ea630653ed347f655c5c63c38bbc8a36f735672
minzziPark <minzzi0068@gmail.com>
2024-03-02 16:23:25 +0900

    add : basic80 추가

---
+++ Programmers/Basics303/Day5/basic80.js
@@ -0,0 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181878
+// 원하는 문자열 찾기
+const solution = (myString, pat) =>
+  myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
